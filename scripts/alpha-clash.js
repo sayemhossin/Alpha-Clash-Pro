188ea05f205a7c507433e6207fde603a987614d9
sayemhossin <sayemhossin22@gmail.com>
2024-02-14 02:09:32 +0600

    first day work done

--- scripts/alpha-clash.js
+++ scripts/alpha-clash.js
@@ -1,3 +1,38 @@
+// function play(){
+//     // step 1 hide the home screen.to hide the screen add the class hidden to the home section
+//   const homeSection = document.getElementById('home-screen')
+//   homeSection.classList.add('hidden')
+// //   console.log(homeSection.classList)
+
+
+//     // step 2 show the playground
+//     const playgroundSection = document.getElementById('play-ground')
+//     playgroundSection.classList.remove('hidden')
+
+//     // console.log(playgroundSection.classList)
+// }
+
+
+function continueGame(){
+    // 1  generate a random alphabet
+    const alphabet = getARandomAlphabet()
+    console.log('your random alphabet', alphabet)
+
+    // set randomly generated alphabet to the screen (show it)
+
+    const currentAlphabetElement = document.getElementById('current-alphabet')
+    currentAlphabetElement.innerText = alphabet
+
+    // set background color
+    setBackgroundColorById(alphabet)
+
+}
+
+
+
+
 function play(){
-    console.log('play start')
+    hideElementById('home-screen')
+    shoeElementById('play-ground')
+    continueGame()
 }
